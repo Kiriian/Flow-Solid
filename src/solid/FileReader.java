f4aebea2dc299c6993335a3225d281873819b2ba
hp <hp@10.50.131.224>
2014-11-11 14:30:02 +0100

    ToString

--- src/solid/FileReader.java
+++ src/solid/FileReader.java
@@ -1,5 +1,9 @@
-
 package solid;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  *
@@ -7,6 +11,39 @@ package solid;
  */
 public class FileReader
 {
-    String hej;
-    String ed;
+    public static ArrayList<Word> load(String WordList) 
+    {
+        Scanner file_scanner = null;
+        ArrayList<Word> WordArray = new ArrayList<Word>();
+
+        try
+        {
+            file_scanner = new Scanner(new File(WordList));  //Connection to the file using the Scanner object
+        } 
+        
+        catch (FileNotFoundException ex)
+        {
+            System.out.println("Could not find the file to load from! Returning null.");
+            ex.printStackTrace();
+            return null;  //If something goes wrong the method returns null
+        }
+
+        System.out.println("File has been found");
+
+        while (file_scanner.hasNextLine()) 
+        {  //File found. Reading one line. 
+            String linje = file_scanner.nextLine();
+            Scanner sc = new Scanner(linje).useDelimiter(",");
+            String danishWord = sc.next();
+            String englishWord = sc.next();
+            
+            
+             Word w = new Word(danishWord, englishWord);
+            
+            WordArray.add(w);  //Reading in a single line and saving in the ArrayList
+        }
+
+        file_scanner.close();  //Closing the file
+        return WordArray;
+    }
 }
