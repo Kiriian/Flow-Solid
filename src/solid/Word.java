6b5047513a5f093c045212cd41d8d16d03311071
hp <hp@10.50.131.224>
2014-11-11 14:24:51 +0100

    har lavet word klassen og tilføjet arrayliste i filereader

---
+++ src/solid/Word.java
@@ -0,0 +1,17 @@
+package solid;
+
+/**
+ * @author Jeanette og Marta
+ */
+
+class Word
+{
+  String danishWord;
+  String englishWord;
+  
+  public Word(String danishWord, String englishWord)
+  {
+      this.danishWord = danishWord;
+      this.englishWord = englishWord;
+  }
+}
